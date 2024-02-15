c8c71ef1b8d07d0e4840a6a0585f1c04b3a3cc73
imthemobin <imthemobin@gmail.com>
2024-02-15 17:09:21 +0330

    add fonts bootstarp files and some pages wwith ejs file

--- app/routes/web/home.js
+++ app/routes/web/home.js
@@ -3,9 +3,13 @@ const router = express.Router();
 
 //controllers
 const homeController = require('app/http/controllers/homeController')
+const loginController = require('app/http/controllers/auth/loginController');
+const registerController = require('app/http/controllers/auth/registerController');
 
 
 //Home Router
 router.get("/", homeController.index);
+router.get('/login' , loginController.showLoginForm);
+router.get('/register' , registerController.showRegsitrationForm);
 
 module.exports = router;
