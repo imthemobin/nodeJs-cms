07ad3907edfb8c51cb94204fcf91352c1c5eddc3
imthemobin <imthemobin@gmail.com>
2024-02-16 12:36:36 +0330

    create register page and validation on this

--- app/http/controllers/auth/registerController.js
+++ app/http/controllers/auth/registerController.js
@@ -2,7 +2,41 @@ const controller = require("app/http/controllers/controller");
 
 class registerController extends controller {
   showRegsitrationForm(req, res) {
-    res.render("auth/register");
+    res.render("auth/register", { messages: req.flash("errors") });
+  }
+
+  registerProcess(req, res, next) {
+    this.validationData(req).then((result) => {
+      if (result) return res.json("register proccess");
+      else res.redirect("/register");
+    });
+  }
+
+  validationData(req) {
+    req.checkBody("name", "فیلد نام نمی تواند خالی باشد").notEmpty();
+    req
+      .checkBody("name", "فیلد نام نمی تواند کمتر از ۵ کاراکتر باشد")
+      .isLength({ min: 5 });
+    req.checkBody("email", "فیلد ایمیل نمی تواند خالی باشد").notEmpty();
+    req.checkBody("email", "فیلد ایمیل معتبر نیست").isEmail();
+    req.checkBody("password", "فیلد پسورد نمی تواند خالی باشد").notEmpty();
+    req
+      .checkBody("password", "فیلد پسورد نمی تواند کمتر از ۸ کاراکتر باشد")
+      .isLength({ min: 8 });
+
+    return req
+      .getValidationResult()
+      .then((result) => {
+        const errors = result.array();
+        const message = [];
+        errors.forEach((error) => message.push(error.msg));
+
+        if (message.length == 0) return true;
+
+        req.flash("errors", message);
+        return false;
+      })
+      .catch((error) => console.log(error));
   }
 }
 
--- app/routes/web/home.js
+++ app/routes/web/home.js
@@ -11,5 +11,6 @@ const registerController = require('app/http/controllers/auth/registerController
 router.get("/", homeController.index);
 router.get('/login' , loginController.showLoginForm);
 router.get('/register' , registerController.showRegsitrationForm);
+router.post('/register' , registerController.registerProcess);
 
 module.exports = router;
