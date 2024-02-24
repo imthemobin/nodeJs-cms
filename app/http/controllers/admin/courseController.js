549586cda39c4dc1e8e9cb9d3368fea93524bf7d
imthemobin <imthemobin@gmail.com>
2024-02-24 12:19:45 +0330

    image resizing{1080,720,480}

--- app/http/controllers/admin/courseController.js
+++ app/http/controllers/admin/courseController.js
@@ -1,6 +1,8 @@
 const controller = require("app/http/controllers/controller");
 const Course = require("app/models/course");
-const fs = require('fs')
+const fs = require("fs");
+const path = require("path");
+const sharp = require("sharp");
 
 class courseController extends controller {
   index(req, res) {
@@ -15,15 +17,12 @@ class courseController extends controller {
     let result = await this.validationData(req);
 
     if (!result) {
-        //if error exist in form image dont created
-        if(req.file){
-            fs.unlink(req.file.path,(error)=>{})
-        }
+      //if error exist in form image dont created
+      if (req.file) {
+        fs.unlink(req.file.path, (error) => {});
+      }
       return this.back(req, res);
     }
-
-
-    // uplode image
 
     // create course
 
@@ -34,7 +33,7 @@ class courseController extends controller {
       body: req.body.body,
       type: req.body.type,
       price: req.body.price,
-      images: req.body.images,
+      images: JSON.stringify(this.imageResize(req.file)),
       tags: req.body.tags,
     });
 
@@ -44,8 +43,36 @@ class courseController extends controller {
   }
 
   slug(title) {
-    return title.replace(/([^۰-۹آ-یa-z0-9]|-)+/g , "-")
-}
+    return title.replace(/([^۰-۹آ-یa-z0-9]|-)+/g, "-");
+  }
+
+  imageResize(image) {
+    const imageInfo = path.parse(image.path);
+
+    let addresImage = {};
+
+    addresImage["original"] = this.getUrlImage(
+      `${image.destination}/${image.filename}`
+    );
+
+    const resize = (size) => {
+      let imageName = `${imageInfo.name}-${size}${imageInfo.ext}`;
+
+      addresImage[size] = this.getUrlImage(`${image.destination}/${imageName}`);
+
+      sharp(image.path)
+        .resize(size, null)
+        .toFile(`${image.destination}/${imageName}`);
+    };
+
+    [1080, 720, 480].map(resize);
+
+    return addresImage;
+  }
+
+  getUrlImage(dir) {
+    return dir.substring(8);
+  }
 }
 
 module.exports = new courseController();
