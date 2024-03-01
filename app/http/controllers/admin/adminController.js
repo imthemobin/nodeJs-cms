5ca93f564a4c35cb7a7d12d03993c25d38f9512e
imthemobin <imthemobin@gmail.com>
2024-03-01 11:58:55 +0330

    add ckdeditor for edit body text

--- app/http/controllers/admin/adminController.js
+++ app/http/controllers/admin/adminController.js
@@ -2,6 +2,15 @@ class adminController{
     index(req , res) {
         res.render('admin/index');
     }
+
+    uploadImage(req,res){
+        let image = req.file;
+        res.json({
+            'uploaded': 1,
+            'fileName': image.fileName,
+            'url': `${image.destination}/${image.filename}`.substring(8)
+        })
+    }
 }
 
 module.exports = new adminController()
